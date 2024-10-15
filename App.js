d7c7c2e370c320cedd4b01703db9bee2cb507aca
LucasHL7 <lhls2002@gmail.com>
2024-10-15 10:40:44 -0300

    att 0.1

---
+++ App.js
@@ -0,0 +1,40 @@
+import {Text, StyleSheet, View, Image} from "react-native";
+import Slider from "@react-native-community/slider";
+
+export default function app(){
+  return(
+    <View style ={styles.container}>
+      <Image source = {require("./src/assets/logo.png")} style={styles.image}/>
+
+      <Text style={styles.title}>
+        10 caracteres
+      </Text>
+
+      <View style={styles.area}> 
+        <Slider style={{height: 50}}
+        minimumValue={6}
+        maximumValue={20}
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles  = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "#010A17",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,  // Defina a largura desejada
+    height: 200,  // Defina a altura desejada
+  },
+  title:{
+    color: "#fff",
+  },
+  area:{
+
+  },
+})
