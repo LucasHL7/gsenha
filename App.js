e5264e23c6f38cb40bf13509a84bdb4ea48e22f2
LucasHL7 <lhls2002@gmail.com>
2024-10-16 09:20:05 -0300

    att 0.2

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
-import {Text, StyleSheet, View, Image} from "react-native";
+import {useState} from "react"
+import {Text, StyleSheet, View, Image, TouchableOpacity} from "react-native";
 import Slider from "@react-native-community/slider";
 
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" 
+
+//funções//
 export default function app(){
+  const [size, setSize]= useState(6)
+   
+  function gsenha(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i <size; i++){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    console.log(pass);
+  }
+
   return(
     <View style ={styles.container}>
       <Image source = {require("./src/assets/logo.png")} style={styles.image}/>
 
       <Text style={styles.title}>
-        10 caracteres
+        {size} caracteres
       </Text>
 
       <View style={styles.area}> 
         <Slider style={{height: 50}}
         minimumValue={6}
         maximumValue={20}
+        value={size}
+        onValueChange={ (value) => setSize(value.toFixed(0))}
         />
       </View>
+
+      <TouchableOpacity style={styles.buttom} onPress={gsenha}>
+        <Text style={styles.text}>Gerar Senha</Text>
+      </TouchableOpacity>
     </View>
   )
 }
-
 
 //estilos//
 const styles  = StyleSheet.create({
@@ -34,9 +54,29 @@ const styles  = StyleSheet.create({
     height: 200,
   },
   title:{
-    color: "#fff",
+    color: "#f3f3fF",
+    fontSize: 20,
+    fontWeight: "bold",
   },
   area:{
-
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#03132B",
+    borderRadius: 8,
+    padding: 8,
   },
+  buttom:{
+    backgroundColor: "#03132B",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius:8,
+    padding: 8,
+  },
+  text:{
+    color: "#F3F3FF",
+    fontSize: 15,
+  }
 })
